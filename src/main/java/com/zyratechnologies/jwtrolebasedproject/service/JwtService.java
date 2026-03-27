a001f5095b91660c57ca774175ce0ee7d6d31f34
Caston mubvumbi <castonmubvumbi@gmail.com>
2026-03-28 01:06:13 +0200

    added a JwtAuthenticationClass and JwtService

--- src/main/java/com/zyratechnologies/jwtrolebasedproject/service/JwtService.java
+++ src/main/java/com/zyratechnologies/jwtrolebasedproject/service/JwtService.java
@@ -1,15 +1,12 @@
 package com.zyratechnologies.jwtrolebasedproject.service;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.io.Decoders;
-
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.function.Function;
@@ -21,7 +18,7 @@ public class JwtService {
     @Value("${jwt.secret}")
     private  String  secret;
     @Value("${jwt.expiration}")
-    private  String  expiration;
+    private  Long expiration;
 
     private SecretKey getKey(){
         byte keyBytes[] = Decoders.BASE64URL.decode(secret);
@@ -43,10 +40,25 @@ public class JwtService {
                 .getBody();
 
     }
-    public <T>T getAllClaims(String token, Function<Claims,T> ressolver){
+    public <T>T extractClaims(String token, Function<Claims,T> resolver){
         Claims claims = extractAllClaims(token);
-        return ressolver.apply(claims);
+        return resolver.apply(claims);
+    }
 
+    public String extractUsername(String token){
+     return extractClaims(token, Claims::getSubject);
+    }
+
+    public Date extractExpiryTime(String token){
+    return extractClaims(token,Claims::getExpiration);
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+       return(extractUsername(token).equals(userDetails.getUsername())&&!isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiryTime(token).before(new Date());
     }
 
 
